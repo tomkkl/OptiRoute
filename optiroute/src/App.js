aecd6d7f1bc3b86cb615914dc137f4b45853e3f6
Ben Lin <benlin2003@gmail.com>
2023-10-01 20:10:17 -0400

    Centered Google Auth, Setup PW Reset Page

--- optiroute/src/App.js
+++ optiroute/src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
+import ResetPassword from './Components/ResetPassword/ResetPassword';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <LoginSignup/>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<LoginSignup />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </>
   );
 }
 
