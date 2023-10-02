a3c7c10ece94c59f0887d0d17f9d98a5b7edc839
Andrew Bradley <andrewcbradley007@gmail.com>
2023-10-01 22:16:18 -0400

    Added Facebook Login

--- optiroute/src/Components/LoginSignup/LoginSignup.jsx
+++ optiroute/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,16 +1,22 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react'
-import './LoginSignup.css'
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import './LoginSignup.css';
 import jwt_decode from "jwt-decode";
-import user_icon from '../Assets/person.png'
-import email_icon from '../Assets/email.png'
-import password_icon from '../Assets/password.png'
+import user_icon from '../Assets/person.png';
+import email_icon from '../Assets/email.png';
+import password_icon from '../Assets/password.png';
 import { useNavigate } from "react-router-dom";
+import { LoginSocialFacebook} from 'reactjs-social-login';
+import {FacebookLoginButton} from "react-social-login-buttons";
 
 const LoginSignup = () => {
     //Navigate to PW reset
     //Change to work with Mongo
     //State does not work as well
+
+  const[profile, setProfile] = useState(null);
+
+
   const [user, setUser ] = useState({});
 
   function handleCallBackResponse(responce){
@@ -55,6 +61,20 @@ const LoginSignup = () => {
             </div>
             <div id = "signInGoogleDiv">
 
+            </div>
+            <div>
+            {!profile ? <LoginSocialFacebook
+                appID="172918275855498"
+                onResolve={(responce) =>{
+                    console.log(responce);
+                    setProfile(responce.data)
+                }}
+                onReject={(error) => {
+                    console.log(error);
+                }}
+            >
+                <FacebookLoginButton/>
+            </LoginSocialFacebook>: ''}
             </div>
         {action ==="Sign Up"?<div ></div>  :<div className="forgot-password">Lost Password? <span>Click Here</span></div>}
         <div className='submit-container'>
