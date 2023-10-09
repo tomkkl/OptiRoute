11e5c7ac2844d7c2c1133631679da67701d4691b
Ben Lin <benlin2003@gmail.com>
2023-10-08 23:44:35 -0400

    Finished homepage, /about, sidebar

--- optiroute/src/Components/Home/Home.jsx
+++ optiroute/src/Components/Home/Home.jsx
@@ -1,49 +1,31 @@
-import "./Home.css";
-import React, { useState } from 'react'
-import { useEffect } from 'react'
-import jwt_decode from "jwt-decode";
-import user_icon from '../Assets/person.png'
-import email_icon from '../Assets/email.png'
-import phone_icon from '../Assets/phone.png'
-import password_icon from '../Assets/password.png'
-import { useNavigate } from 'react-router-dom';
-import UserDetails from "../UserDetails/UserDetails";
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import design from '../Assets/design.png'
+import Sidebar from '../Sidebar/Sidebar';
+import logo from '../Assets/logo.png'
+
+import './Home.css';
+import Description from '../Description/Description';
 
 const Home = () => {
-    // Navigate to PW reset
   const navigate = useNavigate();
-    //Change to work with Mongo
-    //State does not work as well
-  const [users, setUsers] = useState(null)
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const responce = await fetch('/api/users')
-      const json = await responce.json()
-
-      if(responce.ok){
-          setUsers(json)
-      }
-    }
-    fetchUsers()
-  }, [])
-
-  const [action,setAction] = useState("Home");
 
   return (
     <div className='container'>
-        <div className="home">
-          <div className="users">
-            {users && users.map((user) =>(
-              <UserDetails key ={user._id} user = {user}/>
-            ))}
-          </div>
+      <Sidebar />
+
+      <div className='header'>
+        <img src={logo} alt="logo" className="logo-image" />
+        <div className='text'>optiroute</div>
+        <Description />
+        <div className='submit-container'>
+          <div className="submit" onClick={() => { navigate("/login") }}>Sign Up Now!</div>
         </div>
-    <div className='submit-container'>
-        <div className={action ==="Home"?"submit":"submit gray"} onClick={()=>{navigate("/")}}>Sign Up Now!</div>
-            </div>
+        <img src={design} alt="App Mockup" className="mockup-image" />
+      </div>
+
     </div>
-  )
-}
+  );
+};
 
 export default Home;
