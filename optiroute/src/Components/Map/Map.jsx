30a194ce029d268fdd37cd64f08680e3b29d8443
Andrew Bradley <andrewcbradley007@gmail.com>
2023-10-22 22:04:24 -0400

    Setting up Google Maps API

--- optiroute/src/Components/Map/Map.jsx
+++ optiroute/src/Components/Map/Map.jsx
@@ -1,17 +1,29 @@
-const Map = () => {
-    
+import React, { useState, useEffect } from "react";
+import Modal from "react-modal";
+import Datetime from "react-datetime";
+import {useGoogleMap, useLoadScript,} from '@react-google-maps/api'
+
+
+export default function Map() {
+
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: 'AIzaSyDxtuA0Hdx5B0t4X3L0n9STcsGeDXNTYXY'
+    })
+
+
+    const [chosenDate, setChosenDate] = useState(new Date());
     return (
         <div>
-            <div className='input'>
-                <input 
-                    id = "DatePicker"
-                />
-                <button>
-                Create Map
-                </button>
+            <div>
+                <label className="label">Chose Date:</label>
+                <Datetime value={chosenDate} onChange={(date) => setChosenDate(date)} />
             </div>
+            <input id = "name" type='text' placeholder='Name' onChange={handleChangeName}
+                value = {name}/>
+            <button>
+            Create Map
+                onClick
+            </button>
         </div>
     );
 }
-
-export default Map;
