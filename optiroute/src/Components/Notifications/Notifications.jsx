3a18192a2ae98edfcd9e65576621de7efbc67ff2
Tom Linn <tomkyawkhinelin@gmail.com>
2023-10-25 00:53:15 -0400

    Made a noti with custom settings

--- optiroute/src/Components/Notifications/Notifications.jsx
+++ optiroute/src/Components/Notifications/Notifications.jsx
@@ -15,6 +15,19 @@ function Notifications() {
   const showWarningNotification = () => {
     toast.warning('Warning notification');
   };
+  const customNotification = () => { // custom settings like dark theme 
+    toast.info('Customized Notification!', {
+      position: "top-left",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+      });
+
+  }
 
   return (
     <div className="notification app">
@@ -22,6 +35,7 @@ function Notifications() {
       <button onClick={showSuccessNotification}>Show Success Notification</button>
       <button onClick={showErrorNotification}>Show Error Notification</button>
       <button onClick={showWarningNotification}>Show Warning Notification</button>
+      <button onClick={customNotification}>Custom Notification</button>
       <ToastContainer />
     </div>
   );
