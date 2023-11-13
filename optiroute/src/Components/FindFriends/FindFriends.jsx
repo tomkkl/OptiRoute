712b09281f7d4bcc33a5bf70088435a83d371ca5
Tom Linn <tomkyawkhinelin@gmail.com>
2023-11-12 19:53:31 -0500

    Search for friends using name, email or phone num

---
+++ optiroute/src/Components/FindFriends/FindFriends.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+const FindFriends = () => {
+  const [searchBy, setSearchBy] = useState('name'); // Default search option is 'name'
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleSearchByChange = (event) => {
+    setSearchBy(event.target.value);
+  };
+
+  const handleSearchInputChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    // Perform the action based on the selected search option and input
+    console.log(`Searching by ${searchBy}: ${searchInput}`);
+    // You may want to send the search criteria to your backend or perform some other action here
+  };
+
+  return (
+    <div>
+      <h2>Find Friends</h2>
+      <form onSubmit={handleSearchSubmit}>
+        <label>
+          Search By:
+          <select value={searchBy} onChange={handleSearchByChange}>
+            <option value="name">Name</option>
+            <option value="email">Email</option>
+            <option value="phoneNumber">Phone Number</option>
+          </select>
+        </label>
+        <br />
+        <label>
+          {`Enter ${searchBy}: `}
+          <input
+            type="text"
+            value={searchInput}
+            onChange={handleSearchInputChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default FindFriends;
